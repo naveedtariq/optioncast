50375d8f163b6e6ad1b336d734cfa75c57c619e1
Zeshan Latif <zeshanlb@gmail.com>
2014-11-06 15:27:15 +0500

    precompile

---
+++ public/assets/custom-0828a409c4eb0e277996d41f5f1fd790.js
@@ -0,0 +1,15 @@
+var ready;
+ready = function() {
+    Main.init();
+    //UISliders.init();
+    //Charts.init();
+    //FormWizard.init();
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
+
+
+//jQuery(document).ready(function() {
+//
+// });
