c89faaa9d2b23127445d7db52f80a21af704e004
Zeshan Latif <zeshanlb@gmail.com>
2014-11-14 16:00:10 +0500

    test

---
+++ app/admin/user_answers.rb
@@ -0,0 +1,27 @@
+ActiveAdmin.register UserAnswer do
+  menu :priority => 9
+
+  index do
+    selectable_column
+    id_column
+    column  :question_id
+    column  :answer_id
+    column  :user_id
+    column  :value
+    actions
+  end
+
+  filter :question_id
+  filter :answer_id
+  filter :user_id
+  filter :value
+
+  form do |f|
+    f.inputs "Answer Details" do
+      f.input :value
+    end
+    f.actions
+  end
+  
+
+end
