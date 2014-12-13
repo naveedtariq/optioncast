e2f0ef0f341dabfb29d20bdc54d03f4b85407fee
zeshan latif <zeshanlb@gmail.com>
2014-12-13 06:21:21 +0500

    Tool tips dropdown added

--- app/admin/tool_tips.rb
+++ app/admin/tool_tips.rb
@@ -12,7 +12,7 @@ ActiveAdmin.register ToolTips do
 
   form do |f|
     f.inputs "Tip Details" do
-      f.input :rank  
+      f.input :rank, :as => :select, :collection => [["RetirementRank","RetirementRank"],["IncomeRank","IncomeRank"],["FinancialRank","FinancialRank"]], :include_blank => false  
       f.input :description
     end
     f.actions
