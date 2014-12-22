338513d366d846d26412210d724e7587d4f26baa
zeshan latif <zeshanlb@gmail.com>
2014-12-22 18:17:33 +0500

    precompile

--- app/assets/javascripts/application2.js
+++ app/assets/javascripts/application2.js
@@ -12,43 +12,15 @@
 //
 
 //= require jquery
-//= require plugins/jquery-ui/jquery-ui-1.10.2.custom.min
 //= require plugins/bootstrap/js/bootstrap.min
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require bootstrap.min
-//= require plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min
-//= require plugins/blockUI/jquery.blockUI
-//= require plugins/iCheck/jquery.icheck.min
-//= require plugins/perfect-scrollbar/src/jquery.mousewheel
-//= require plugins/perfect-scrollbar/src/perfect-scrollbar
-//= require plugins/less/less-1.5.0.min
-//= require plugins/jquery-cookie/jquery.cookie
-//= require plugins/bootstrap-colorpalette/js/bootstrap-colorpalette
 //= require main
-//=require plugins/flot/jquery.flot
-//=require plugins/flot/jquery.flot.pie
-//=require plugins/flot/jquery.flot.resize
-//=require plugins/jquery.sparkline/jquery.sparkline
-//=require plugins/jquery-easy-pie-chart/jquery.easy-pie-chart
-//=require plugins/jquery-ui-touch-punch/jquery.ui.touch-punch
-//=require plugins/flot/jquery.flot.categories
-//=require plugins/flot/jquery.flot.pie
 //=require charts
-//=require plugins/fullcalendar/fullcalendar/fullcalendar
 
-//= require plugins/jquery-ui/jquery-ui-1.10.1.custom.min
-//= require plugins/jQRangeSlider/jQAllRangeSliders-min
-//= require plugins/jQuery-Knob/js/jquery.knob
-//= require ui-sliders
 //= require turbolinks
-//= require index
-//= require plugins/jquery-validation/dist/jquery.validate.min
-//= require plugins/jQuery-Smart-Wizard/js/jquery.smartWizard
-//=require form-wizard
 //=require custom
-//= require plugins/respond.min
-//= require plugins/excanvas.min
 
 //= require bootstrap-datepicker
 
