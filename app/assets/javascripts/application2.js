bc6b723bac2e66c230f0a0661773183a42eae7ac
zeshan latif <zeshanlb@gmail.com>
2014-12-22 17:56:58 +0500

    date picker issue

---
+++ app/assets/javascripts/application2.js
@@ -0,0 +1,55 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+
+//= require jquery
+//= require plugins/jquery-ui/jquery-ui-1.10.2.custom.min
+//= require plugins/bootstrap/js/bootstrap.min
+//= require jquery_ujs
+//= require jquery.ui.all
+//= require bootstrap.min
+//= require plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min
+//= require plugins/blockUI/jquery.blockUI
+//= require plugins/iCheck/jquery.icheck.min
+//= require plugins/perfect-scrollbar/src/jquery.mousewheel
+//= require plugins/perfect-scrollbar/src/perfect-scrollbar
+//= require plugins/less/less-1.5.0.min
+//= require plugins/jquery-cookie/jquery.cookie
+//= require plugins/bootstrap-colorpalette/js/bootstrap-colorpalette
+//= require main
+//=require plugins/flot/jquery.flot
+//=require plugins/flot/jquery.flot.pie
+//=require plugins/flot/jquery.flot.resize
+//=require plugins/jquery.sparkline/jquery.sparkline
+//=require plugins/jquery-easy-pie-chart/jquery.easy-pie-chart
+//=require plugins/jquery-ui-touch-punch/jquery.ui.touch-punch
+//=require plugins/flot/jquery.flot.categories
+//=require plugins/flot/jquery.flot.pie
+//=require charts
+//=require plugins/fullcalendar/fullcalendar/fullcalendar
+
+//= require plugins/jquery-ui/jquery-ui-1.10.1.custom.min
+//= require plugins/jQRangeSlider/jQAllRangeSliders-min
+//= require plugins/jQuery-Knob/js/jquery.knob
+//= require ui-sliders
+//= require turbolinks
+//= require index
+//= require plugins/jquery-validation/dist/jquery.validate.min
+//= require plugins/jQuery-Smart-Wizard/js/jquery.smartWizard
+//=require form-wizard
+//=require custom
+//= require plugins/respond.min
+//= require plugins/excanvas.min
+
+//= require bootstrap-datepicker
+
+//require_tree .
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,2 @@
 Rails.application.config.assets.precompile += %w( application_index.css application_index.js )
+Rails.application.config.assets.precompile += %w( application2.css application2.js )
